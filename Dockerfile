6137071043926084b8b5848a8903d9af3d52f572
churon17 <jeancarlosalarguez16@gmail.com>
2020-04-07 16:22:33 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+#BUILD PHASE
+FROM node:alpine as builder
+
+WORKDIR /app
+
+COPY package.json .
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+#/app/build <-- all the stuff
+
+FROM nginx
+
+COPY --from=builder /app/build /usr/share/nginx/html
+
+
+
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jean Carlos Alarcón is the Bosssss!
         </p>
         <a
           className="App-link"
