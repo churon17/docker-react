c6c086c3e81c4df99feebfc614278b7a1fa8fbe9
churon17 <jeancarlosalarguez16@gmail.com>
2020-04-07 22:04:36 -0500

    Changed App text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Angular is better than Angulaaar!
         </a>
       </header>
     </div>
